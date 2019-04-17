3a9eae1d7fa85ee69dae52537f70ac4563b46252
Olesya <magalesenok@gmail.com>
2019-04-17 21:20:42 +0200

    калькулятор JS

--- js/script.js
+++ js/script.js
@@ -204,37 +204,37 @@ let slideIndex = 1,
         daysSum = 0,
         total = 0;
 
-        totalValue.innerHTML = 0;
+    totalValue.innerHTML = 0;
 
-        persons.addEventListener('change', function() {
-            personsSum = +this.value;
-            total = (daysSum + personsSum)*4000;
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum)*4000;
 
-            if(restDays.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                totalValue.innerHTML = total;
-            }
-        });
-        restDays.addEventListener('change', function() {
-            daysSum = +this.value;
-            total = (daysSum + personsSum)*4000;
+        if(restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+             totalValue.innerHTML = total;
+        }
+    });
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum)*4000;
 
-            if(persons.value == '') {
-                totalValue.innerHTML = 0;
-            } else {
-                totalValue.innerHTML = total;
-            }
-        });
+        if(persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
 
-        place.addEventListener('change', function() {
-           if (restDays.value == '' || persons.value == '') {
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
                totalValue.innerHTML = 0;
-           } else {
-               let a = total;
-               totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-           }
-        });
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 });
 // Второе задание
 
