e002641316fdbe480ed34b045e658b87dfb010b2
Olesya <magalesenok@gmail.com>
2019-04-17 12:54:34 +0200

    форма -  JSON

--- js/script.js
+++ js/script.js
@@ -116,15 +116,22 @@ window.addEventListener('DOMContentLoaded', function() {
 
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
         let formData = new FormData(form);
-        request.send(formData);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
 
         request.addEventListener('readystatechange', function() {
             if (request.readyState < 4) {
                 statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
+            } else if(request.readyState === 4 && request.status == 200) {
                 statusMessage.innerHTML = message.success;
             } else {
                 statusMessage.innerHTML = message.failure;
@@ -135,6 +142,8 @@ window.addEventListener('DOMContentLoaded', function() {
             input[i].value = '';
         }
     });
+
+
 });
 // Второе задание
 
